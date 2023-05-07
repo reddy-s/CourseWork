5e06d82784066abc9cf37a054f1a2f5a58aea461
Sangram Reddy <reddy.horcrux@gmail.com>
2023-05-07 22:03:01 +0100

    Additional models

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -8,7 +8,7 @@ from .resnet import (
     resnet50,
     resnet50_fc512,
 )
-from .tvmodels import mobilenet_v3_small, vgg16, googLeNet
+from .tvmodels import mobilenet_v3_small, vgg16, alex_net
 
 
 __model_factory = {
@@ -21,7 +21,7 @@ __model_factory = {
     "resnet50_fc512": resnet50_fc512,
     "mobilenet_v3_small": mobilenet_v3_small,
     "vgg16": vgg16,
-    "googLeNet": googLeNet
+    "alex_net": alex_net
 }
 
 
