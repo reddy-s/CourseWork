144260644276f756560141c62874025c84c1ebd6
Sangram Reddy <reddy.horcrux@gmail.com>
2023-05-08 22:16:52 +0100

    Additional models

--- train.sh
+++ train.sh
@@ -3,14 +3,14 @@
 python main.py \
 -s veri \
 -t veri \
--a resnet18_fc512 \
+-a resnet50_fc512 \
 --root /Users/sangramreddy/Documents/dev/pocs \
 --height 224 \
 --width 224 \
 --gpu-devices 16 \
 --optim amsgrad \
 --lr 0.003 \
---max-epoch 30 \
+--max-epoch 1 \
 --stepsize 20 40 \
 --train-batch-size 64 \
 --test-batch-size 100 \
